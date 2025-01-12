d876da4c0d1272c831517418216be8e0c3beb83b
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2025-01-12 16:00:08 +0500

    Initial backend commit

---
+++ backend/models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
+
+// export const User = mongoose.model("User", userSchema);
